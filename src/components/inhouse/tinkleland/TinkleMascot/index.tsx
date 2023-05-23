d43110d0fcc2f57a90c9bbfd98d63a37d868efa4
소망 이 <lsm@thetelos.net>
2023-05-23 11:46:04 +0900

    팅클랜드 마스코트 부분 수정 작업 완료

--- src/components/inhouse/tinkleland/TinkleMascot/index.tsx
+++ src/components/inhouse/tinkleland/TinkleMascot/index.tsx
@@ -1,60 +1,72 @@
 import React, { useState } from 'react'
 import cheeseInfo from 'assets/images/inhouse/tinkleland/tinkle_cheese_info.png'
 import cheeseInfoMobile from 'assets/images/inhouse/tinkleland/tinkle_cheese_info_mobile.png'
-import cheeseNavSelect from 'assets/images/inhouse/tinkleland/tinkle_nav_cheese_select.png'
-import cookieNav from 'assets/images/inhouse/tinkleland/tinkle_nav_cookie.png'
-import clsx from 'clsx'
+import cheeseActive from 'assets/images/inhouse/tinkleland/tinkle_cheese_active.png'
+import cheeseInactive from 'assets/images/inhouse/tinkleland/tinkle_cheese_inactive.png'
+import cookieInfo from 'assets/images/inhouse/tinkleland/tinkle_cookie_info.png'
+import cookieInfoMobile from 'assets/images/inhouse/tinkleland/tinkle_cookie_info_mobile.png'
+import cookieActive from 'assets/images/inhouse/tinkleland/tinkle_cookie_active.png'
+import cookieInactive from 'assets/images/inhouse/tinkleland/tinkle_cookie_inactive.png'
 
 const TinkleMascot = () => {
-  const [currentNav, setCurrentNav] = useState('cheese')
+  const [currentNav, setCurrentNav] = useState('cookie')
 
   return (
     <section className="relative bg-white">
       <div>
-        <img
-          src={cheeseInfo}
-          alt="팅클랜드 치즈 소개"
-          className="hidden object-cover w-full sm:h-300 md:h-350 lg:h-500 sm:block"
-        />
-        <img
-          src={cheeseInfoMobile}
-          alt="팅클랜드 치즈 소개"
-          className="block sm:hidden"
-        />
+        {currentNav === 'cheese' ? (
+          <>
+            <img
+              src={cheeseInfo}
+              alt="팅클랜드 치즈 소개"
+              className="hidden object-cover w-full sm:h-300 md:h-350 lg:h-500 sm:block"
+            />
+            <img
+              src={cheeseInfoMobile}
+              alt="팅클랜드 치즈 소개"
+              className="block sm:hidden"
+            />
+          </>
+        ) : (
+          currentNav === 'cookie' && (
+            <>
+              <img
+                src={cookieInfo}
+                alt="팅클랜드 치즈 소개"
+                className="hidden object-cover w-full sm:h-300 md:h-350 lg:h-500 sm:block"
+              />
+              <img
+                src={cookieInfoMobile}
+                alt="팅클랜드 치즈 소개"
+                className="block sm:hidden"
+              />
+            </>
+          )
+        )}
       </div>
-      <div className="w-full absolute top-[65%] sm:top-[50%] translate-y-[-50%]">
-        <ul className="w-[35%] sm:w-[25%] max-w-320 flex flex-col gap-20 under:rounded-[0_15px_15px_0] under:h-110 under:md:h-140 under:lg:h-200 under:pt-10 under:flex-center under:flex-col under:sm:gap-5 under:cursor-pointer">
-          <li
-            className={clsx(
-              currentNav === 'cookie'
-                ? 'bg-[#7C4A2C] under:text-white w-full'
-                : 'bg-[#D6C0A6] under:text-[#EBDAC6] w-[70%]',
-              'transition-all duration-500',
+      <div className="w-full absolute top-[30%] sm:top-[50%] translate-y-[-50%]">
+        <ul className="under:cursor-pointer max-w-90 sm:max-w-120 md:max-w-140 lg:max-w-200">
+          <li onClick={() => setCurrentNav('cheese')}>
+            {currentNav === 'cheese' ? (
+              <img src={cheeseActive} alt="쿠키" className="w-full" />
+            ) : (
+              <img
+                src={cheeseInactive}
+                alt="쿠키"
+                className="w-[85%] lg:w-auto"
+              />
             )}
-            onClick={() => setCurrentNav('cookie')}
-          >
-            <img
-              src={cookieNav}
-              alt="쿠키"
-              className="w-70 md:w-80 lg:w-auto"
-            />
-            <p className="font-bold md:text-18 lg:text-20">쿠키</p>
           </li>
-          <li
-            className={clsx(
-              currentNav === 'cheese'
-                ? 'bg-[#7C4A2C] under:text-white w-full'
-                : 'bg-[#D6C0A6] under:text-[#EBDAC6] w-[70%]',
-              'transition-all duration-500',
+          <li onClick={() => setCurrentNav('cookie')}>
+            {currentNav === 'cookie' ? (
+              <img src={cookieActive} alt="쿠키" className="w-full" />
+            ) : (
+              <img
+                src={cookieInactive}
+                alt="쿠키"
+                className="w-[85%] lg:w-auto"
+              />
             )}
-            onClick={() => setCurrentNav('cheese')}
-          >
-            <img
-              src={cheeseNavSelect}
-              alt="치즈"
-              className="w-70 md:w-80 lg:w-auto"
-            />
-            <p className="font-bold text-white md:text-18 lg:text-20">치즈</p>
           </li>
         </ul>
       </div>
