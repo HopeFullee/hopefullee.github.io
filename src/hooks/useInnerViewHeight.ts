9aa57b85157f3cf2fca343df2d99fa0b505ddd1b
lsm <lsm@thetelos.net>
2023-06-03 23:03:03 +0900

    Updates

--- src/hooks/useInnerViewHeight.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react'
-
-export const useInnerViewHeight = () => {
-  let vh = 0
-  useEffect(() => {
-    vh = window.innerHeight * 0.01
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
-  }, [])
-}
--- src/store/storeScrollPosition.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil'
-
-export const scrollPositionStateAtom = atom({
-  key: 'scrollPositionState',
-  default: 0,
-})
